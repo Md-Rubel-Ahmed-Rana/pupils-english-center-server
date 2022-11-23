688c648e31e48ae51f49d236dff5be789b4895ec
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-11-23 15:34:49 +0600

    Repo made public

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
 app.use(cors());
 
 const courses = require("./data/courses.json")
